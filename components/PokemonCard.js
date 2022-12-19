03b9e62c51604f61abd9e4aa76f690d6c4fa0b5d
unknown <onuralphachamzaoglu@gmail.com>
2022-12-20 00:47:44 +0300

    first commit

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -27,7 +27,7 @@ const PokemonCard = (props) => {
       <div
         onClick={async () => {
           await getDetails();
-          console.log()
+         
          
           setIsModal(!isModal);
         }}
@@ -38,58 +38,7 @@ const PokemonCard = (props) => {
 
       {isModal && (
         <PokemonDetail isModal={isModal} setIsModal={setIsModal} details ={details} />
-        // <>
-        //   <div
-        //     onClick={() => {
-        //       setIsModal(false);
-        //     }}
-        //     className="fixed top-0 w-screen h-screen bg-black bg-opacity-60 z-10 "
-        //   ></div>
-        //   <div className="fixed mx-auto my-auto w-[340px] p-2   md:w-[900px] sm:p-10  z-20 bg-white rounded-xl">
-        //     <div className="flex flex-row w-full  justify-end">
-        //       <button
-        //         onClick={() => {
-        //           setIsModal(false);
-        //         }}
-        //         className="btn btn-ghost text-red-500  text-3xl font-bold"
-        //       >
-        //         X
-        //       </button>
-        //     </div>
-        //     <div className="flex max-md:justify-center flex-wrap    border-b-2 pb-2 text-stone-900 ">
-        //       <div className="flex flex-col gap-2">
-        //         <Image
-        //           className="ml-5 w-auto max-w-[200px]"
-        //           src={details.sprites.other.dream_world.front_default}
-        //           width={150}
-        //           height={150}
-        //           placeholder="blur"
-        //           blurDataURL={details.sprites.front_default}
-        //           alt="pokemon picture"
-        //         />
-        //         <h1 className="capitalize text-5xl w-full font-semibold te ">
-        //           {details.name}
-        //         </h1>
-        //         <span className=" ">
-        //           <span className="font-bold">Weight:</span> {details.weight} kg
-        //         </span>
-        //         <span className="">
-        //           <span className="font-bold">Height:</span> {details.height} m
-        //         </span>
-        //         <div className="flex gap-2 ">
-        //           {details.types.map((type) => (
-        //             <span
-        //               key={Math.random()}
-        //               className={`pokemon-type px-4 py-1 rounded capitalize text-gray-100 font-semibold  ${type.type.name}`}
-        //             >
-        //               {type.type.name}
-        //             </span>
-        //           ))}
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </>
+       
       )}
     </>
   );
